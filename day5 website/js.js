40312b66d35ef71fd239142a91bc77da102eb64b
ADHITHYA2004 <adhithyaadhithya316@gmail.com>
2025-01-01 23:28:54 +0530

    website membership login

---
+++ day5 website/js.js
@@ -0,0 +1,11 @@
+// var page = document.getElementById(drop);
+const page_show = document.getElementById(dropdown-content);
+
+function show(){
+    if(page_show.style.display === 'block'){
+        page_show.style.display = 'none'
+    }
+    else{
+        page_show.style.display = 'block';
+    }
+};
