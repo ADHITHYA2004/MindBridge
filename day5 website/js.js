fc61efb8fa8b269b634e8d6439fb9b4631169a37
ADHITHYA2004 <adhithyaadhithya316@gmail.com>
2025-01-02 20:41:12 +0530

    website completed

--- day5 website/js.js
+++ day5 website/js.js
@@ -1,11 +1,24 @@
-// var page = document.getElementById(drop);
-const page_show = document.getElementById(dropdown-content);
-
+// dropdown page
+const page_show = document.getElementById("dropdown-content");
 function show(){
-    if(page_show.style.display === 'block'){
-        page_show.style.display = 'none'
+    if(page_show.style.display == 'block'){
+        page_show.style.display = 'none';
     }
     else{
         page_show.style.display = 'block';
     }
 };
+
+// open membership login
+const body1=document.getElementsByTagName("body")
+const member_login_show =document.getElementById("membership-login");
+function show_memberlogin(){
+    member_login_show.style.display='block';
+    body1[0].style.backgroundColor='#333d3366';
+};
+
+// to close membership login
+function close_membership_login(){
+
+    member_login_show.style.display='none';
+}
